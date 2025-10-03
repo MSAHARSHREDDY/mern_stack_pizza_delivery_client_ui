a599960af2a60b5e50439838f759d1619ff64b79
MSAHARSHREDDY <saharshreddym59@gmail.com>
2025-10-03 21:35:15 +0530

    add page modification

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,15 +6,15 @@ import { Suspense } from 'react';
 export default async function Home({ searchParams }: { searchParams: { restaurantId: string } }) {
     return (
         <>
-            <section className="bg-white">
-                <div className="container flex flex-col md:flex-row items-start md:items-center justify-between py-24 gap-12">
+            <section className="bg-white min-h-screen flex items-center">
+                <div className="container flex flex-col md:flex-row items-center justify-center gap-12">
                     {/* Left: Text content */}
-                    <div>
+                    <div className="text-center md:text-left">
                         <h1 className="text-7xl font-black font-sans leading-tight">
                             Super Delicious Pizza in <br />
                             <span className="text-primary">Only 45 Minutes!</span>
                         </h1>
-                        <p className="text-2xl mt-8 max-w-lg leading-snug">
+                        <p className="text-2xl mt-8 max-w-lg leading-snug mx-auto md:mx-0">
                             Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
                         </p>
                         <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
